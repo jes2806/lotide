eb33bd97b8e994955784ad87a0a3e3e179c46757
John Sweeney <jes2806@gmail.com>
2022-07-26 20:26:41 -0600

    changing and adding files

--- eqArrays.js
+++ eqArrays.js
@@ -7,13 +7,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function([array1], [array2]) {
-  if (array1 === array2) {
-    return true;
-  }
-  if (array1 !== array2) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
