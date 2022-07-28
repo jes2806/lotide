a2b3272f16ce50d77731c14e4ec03d233d6fb3a3
John Sweeney <jes2806@gmail.com>
2022-07-27 19:31:42 -0600

    adding to lotide project

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// return all the indices (zero-based positions) in the string where each character is found.
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (i === " ") {
+      continue;
+    }
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+console.log(letterPositions("hello"));
+
+// try removing continue later
+
+assertArraysEqual(letterPositions("hello").e, [1]);
