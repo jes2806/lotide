29fc640d435a30019bef828db00ecb7979678ae4
John Sweeney <jes2806@gmail.com>
2022-07-28 20:10:52 -0600

    adding findKey.js to repo

---
+++ findKey.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const data1 = {
+  "Testing": { info: 1 },
+  "Still Testing": { info: 3 },
+  "All the Testing": { info: 2 }
+};
+
+const results1 = findKey(data1, x => x.info === 2);
+assertEqual(results1, "All the Testing");
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
