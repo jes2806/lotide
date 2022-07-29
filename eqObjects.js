64f4952cb1bff738b3c8ba8792c1fdf084d79bee
John Sweeney <jes2806@gmail.com>
2022-07-28 21:34:01 -0600

    adding to lotide

---
+++ eqObjects.js
@@ -0,0 +1,48 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const obj1 = Object.keys(object1);
+  const obj2 = Object.keys(object2);
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+  for (const keys of obj1) {
+    if (object1[keys] !== object2[keys]) {
+      if (Array.isArray(object1[keys])) {
+        if (!eqArrays(object1[keys], object2[keys])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual((eqObjects(cd, dc)), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual((eqObjects(cd, cd2)), false); // => false
