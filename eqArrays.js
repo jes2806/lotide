dc270d4d71fc7dd0ba4ad3f197724cd0e0a3eee7
John Sweeney <jes2806@gmail.com>
2022-08-01 10:48:41 -0600

    adding module functions for dryer code

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,4 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+module.exports =eqArrays;
